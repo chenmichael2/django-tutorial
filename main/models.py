ce2968ec6b842831771fc9f00d6ffc948b1ce5c4
chenmichael2 <chenmichael2012@gmail.com>
2022-02-20 15:49:16 -0800

    commiting Item todolist

--- main/models.py
+++ main/models.py
@@ -2,3 +2,9 @@ from django.db import models
 
 class ToDoList(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
