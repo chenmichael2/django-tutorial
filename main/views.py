be67b88e48b6f2438f6323ad10b567a166d299c1
chenmichael2 <chenmichael2012@gmail.com>
2022-02-20 16:08:57 -0800

    seeing the text items

--- main/views.py
+++ main/views.py
@@ -4,5 +4,7 @@ from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response, id):
-    return HttpResponse("<h1>%s<h1>" % id)
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
+    items = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s<h1><br></br><p>%s</p>" %(ls.name, str(items.text)))
