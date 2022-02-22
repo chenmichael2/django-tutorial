d8cac225ff6394361b9464e5e6dfa1b37ff7fda8
chenmichael2 <chenmichael2012@gmail.com>
2022-02-21 22:30:26 -0800

    commiting cleaning up

--- main/views.py
+++ main/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
 
-
 def index(response, name):
     ls = ToDoList.objects.get(name=name)
     items = ls.item_set.get(id=1)
