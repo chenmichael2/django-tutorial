42f5563f8ec64fbc39d93bf480383231ca6dcc0f
chenmichael2 <chenmichael2012@gmail.com>
2022-02-21 22:30:12 -0800

    cleaning up

--- main/views.py
+++ main/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
 
-# Create your views here.
 
 def index(response, name):
     ls = ToDoList.objects.get(name=name)
