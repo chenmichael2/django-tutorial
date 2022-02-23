6581d1061c68eb78243f692a5b46cae14bd295b3
chenmichael2 <chenmichael2012@gmail.com>
2022-02-22 17:36:08 -0800

    commiting for loop in views

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from .models import ToDoList, Item
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    return render(response, "main/base.html", {"ls":ls})
+    return render(response, "main/list.html", {"ls":ls})
 
 def home(response):
     return render(response, "main/home.html", {"name": "test"})
