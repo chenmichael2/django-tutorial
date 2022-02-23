176c024353c00b4e8a70f342c82219523bb067e2
chenmichael2 <chenmichael2012@gmail.com>
2022-02-22 17:06:58 -0800

    commiting change to id

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ToDoList, Item
 
-def index(response, name):
-    ls = ToDoList.objects.get(name=name)
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
     items = ls.item_set.get(id=1)
     return HttpResponse("<h1>%s<h1><br></br><p>%s</p>" %(ls.name, str(items.text)))
