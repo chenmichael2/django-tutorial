a70873f22c5ebfc16ab746ea656c65f9b61a02f5
chenmichael2 <chenmichael2012@gmail.com>
2022-02-22 17:31:13 -0800

    pushing and finishing list name

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from .models import ToDoList, Item
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    return render(response, "main/base.html", {"name":ls.name})
+    return render(response, "main/base.html", {"ls":ls})
 
 def home(response):
     return render(response, "main/home.html", {"name": "test"})
