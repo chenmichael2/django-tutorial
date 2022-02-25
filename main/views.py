e9c24992670183d181f063214ba5eb71973dde6e
chenmichael2 <chenmichael2012@gmail.com>
2022-02-24 20:04:42 -0800

    if you press the button save

--- main/views.py
+++ main/views.py
@@ -7,7 +7,8 @@ def index(response, id):
     ls = ToDoList.objects.get(id=id)
 
     if response.method == "POST":
-        
+        if response.POST.get("save"):
+            
 
     return render(response, "main/list.html", {"ls":ls})
 
