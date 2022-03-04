7fc2c5c2731b946e6ce07554460df148fc37e05a
chenmichael2 <chenmichael2012@gmail.com>
2022-03-03 16:17:31 -0800

    finishing path for register

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,5 @@ from register import views as v
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
-    path("register/", )
+    path("register/", v.register, name="register"),
 ]
