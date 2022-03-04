21d1cd06ba75486ccd29970fa614db4058903944
chenmichael2 <chenmichael2012@gmail.com>
2022-03-03 17:17:11 -0800

    adding a redirect to home

--- register/views.py
+++ register/views.py
@@ -8,6 +8,7 @@ def register(response):
         form = UserCreationForm(response.POST)
         if form.is_valid():
             form.save()
+        return redirect("/home")
     else:
         form = UserCreationForm()
     
