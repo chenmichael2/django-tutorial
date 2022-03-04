84c1e2c1bcdaa933cc7d8b1445407dc14b370915
chenmichael2 <chenmichael2012@gmail.com>
2022-03-03 17:21:08 -0800

    updating more

--- register/forms.py
+++ register/forms.py
@@ -1,9 +1,11 @@
-from django.models import models
+from django import forms
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
-    email = models.EmailField()
+    email = forms.EmailField()
 
-    class Meta: 
-        
+    class Meta:
+        model = User
+        fields = []
